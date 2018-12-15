9a30543614172ba70b9e67b88191e84a71d33ad8
kostintyn.yavorovenko <kostintyn.yavorovenko@nure.ua>
2018-12-15 22:37:46 +0300

    Added form to browse.jsp

--- usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/web/BrowseServlet.java
@@ -14,6 +14,8 @@ import java.util.Collection;
 public class BrowseServlet extends HttpServlet {
 
     private static final long serialVersionUID = -4683563138736520242L;
+    private static final String ATTR_USERS = "users";
+    private static final String BROWSE_PAGE = "/browse.jsp";
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,8 +26,8 @@ public class BrowseServlet extends HttpServlet {
         Collection<User> collectionOfUsers;
         try {
             collectionOfUsers = DaoFactory.getInstance().getUserDao().findAll();
-            req.getSession().setAttribute("users", collectionOfUsers);
-            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+            req.getSession().setAttribute(ATTR_USERS, collectionOfUsers);
+            req.getRequestDispatcher(BROWSE_PAGE).forward(req, resp);
         } catch (DatabaseException e) {
             throw new ServletException("Can not get users from DB", e);
         }
