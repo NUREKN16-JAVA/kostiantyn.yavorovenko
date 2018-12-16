5e0000113477d7e8cf2180c66b258dafbc20fd21
kostintyn.yavorovenko <kostintyn.yavorovenko@nure.ua>
2018-12-16 10:43:23 +0300

    Updated SearchAgent, added SearchRequestbehaviour, SearchException

--- usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/agent/SearchAgent.java
@@ -1,6 +1,13 @@
 package ua.nure.kn.yavorovenko.usermanagement.agent;
 
+import jade.core.AID;
 import jade.core.Agent;
+import ua.nure.kn.yavorovenko.usermanagement.User;
+import ua.nure.kn.yavorovenko.usermanagement.db.DaoFactory;
+import ua.nure.kn.yavorovenko.usermanagement.db.DatabaseException;
+
+import java.util.Collection;
+
 
 public class SearchAgent extends Agent {
     @Override
@@ -13,5 +20,22 @@ public class SearchAgent extends Agent {
     protected void takeDown() {
         super.takeDown();
         System.out.println(getAID().getName() + " terminated.");
+    }
+
+    public void search(String firstName, String lastName) throws SearchException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            if (users.size() > 0) {
+                showUsers(users);
+            } else {
+                addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName ));
+            }
+        } catch (DatabaseException e) {
+            throw new SearchException(e);
+        }
+    }
+
+    public void showUsers(Collection<User> users) {
+
     }
 }
