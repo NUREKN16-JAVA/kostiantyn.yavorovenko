37a58ec276f473f303e075f3c6ebcdc57d048e72
kostintyn.yavorovenko <kostintyn.yavorovenko@nure.ua>
2018-12-16 03:18:58 +0300

    Updated EditServletTest and EditServlet

--- usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/web/EditServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/web/EditServlet.java
@@ -3,6 +3,7 @@ package ua.nure.kn.yavorovenko.usermanagement.web;
 import ua.nure.kn.yavorovenko.usermanagement.User;
 import ua.nure.kn.yavorovenko.usermanagement.db.DaoFactory;
 import ua.nure.kn.yavorovenko.usermanagement.db.DatabaseException;
+import ua.nure.kn.yavorovenko.usermanagement.web.exceptions.ValidationException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,29 +15,38 @@ import java.text.ParseException;
 
 public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = -2274888176643905731L;
+    private static final String EDIT_PAGE = "/edit.jsp";
+    private static final String BROWSE_PAGE = "/browse.jsp";
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
         if (req.getParameter("okButton") != null) {
             doOk(req, resp);
-        }else if (req.getParameter("cancelButton") != null) {
+        } else if (req.getParameter("cancelButton") != null) {
             doCancel(req, resp);
-        }else{
-            showPage();
+        } else {
+            showPage(req, resp);
         }
     }
 
-    private void showPage() {
-
+    private void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(EDIT_PAGE).forward(req, resp);
     }
 
-    private void doCancel(HttpServletRequest req, HttpServletResponse resp) {
-
+    private void doCancel(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(BROWSE_PAGE).forward(req, resp);
     }
 
     private void doOk(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = getUserFromRequest(req);
+        User user = null;
+        try {
+            user = getUserFromRequest(req);
+        } catch (ValidationException e) {
+            req.setAttribute("error", e.getMessage());
+            showPage(req, resp);
+            return;
+        }
         try {
             processUser(user);
         } catch (DatabaseException e) {
@@ -46,29 +56,42 @@ public class EditServlet extends HttpServlet {
     }
 
     private void processUser(User user) throws DatabaseException {
-            DaoFactory.getInstance().getUserDao().update(user);
+        DaoFactory.getInstance().getUserDao().update(user);
     }
 
-    public User getUserFromRequest(HttpServletRequest req) {
+    public User getUserFromRequest(HttpServletRequest req) throws ValidationException {
+        User user = new User();
         String idString = req.getParameter("id");
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
         String dateOfBirthString = req.getParameter("dateOfBirth");
 
-        User user = null;
-
-        if (idString != null || firstName != null || lastName != null || dateOfBirthString != null) {
-            try {
-                user = new User(
-                        new Long(idString),
-                        firstName,
-                        lastName,
-                        DateFormat.getDateInstance().parse(dateOfBirthString)
-                );
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
+        if (idString == null) {
+            throw new ValidationException("ID is empty!");
         }
+
+        if (firstName == null) {
+            throw new ValidationException("First name is empty!");
+        }
+
+        if (lastName == null) {
+            throw new ValidationException("Last name is empty!");
+        }
+
+        if (dateOfBirthString == null) {
+            throw new ValidationException("Date of birth is empty!");
+        }
+
+        user.setId(new Long(idString));
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+
+        try {
+            user.setDateOfBirth(DateFormat.getDateInstance().parse(dateOfBirthString));
+        } catch (ParseException e) {
+            throw new ValidationException("Date format is incorrect!", e);
+        }
+
         return user;
     }
 }
--- usermanagement/src/test/java/ua/nure/kn/yavorovenko/usermanagement/web/EditServletTest.java
+++ usermanagement/src/test/java/ua/nure/kn/yavorovenko/usermanagement/web/EditServletTest.java
@@ -11,6 +11,8 @@ public class EditServletTest extends MockServletTestCase {
     private static final String FIRST_NAME_FOR_TUSER = "John";
     private static final String LAST_NAME_FOR_TUSER = "Doe";
     private static final Date DATE_OF_BIRTHDAY_FOR_TUSER = new Date();
+    private static final String OK_OPTION = "Ok";
+    private static final String ERROR_ATTRIBUTE = "error";
 
     @Override
     public void setUp() throws Exception {
@@ -32,7 +34,48 @@ public class EditServletTest extends MockServletTestCase {
         addRequestParameter("firstName", FIRST_NAME_FOR_TUSER);
         addRequestParameter("lastName", LAST_NAME_FOR_TUSER);
         addRequestParameter("dateOfBirth", DateFormat.getDateInstance().format(DATE_OF_BIRTHDAY_FOR_TUSER));
-        addRequestParameter("okButton", "Ok");
+        addRequestParameter("okButton", OK_OPTION);
         doPost();
+    }
+
+    public void testEditEmptyFirstName() {
+        addRequestParameter("id", ID_FOR_TUSER_STRING);
+        addRequestParameter("lastName", LAST_NAME_FOR_TUSER);
+        addRequestParameter("dateOfBirth", DateFormat.getDateInstance().format(DATE_OF_BIRTHDAY_FOR_TUSER));
+        addRequestParameter("okButton", OK_OPTION);
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute(ERROR_ATTRIBUTE);
+        assertNotNull("The session scope must have a error message", errorMessage);
+    }
+
+    public void testEditEmptyLastName() {
+        addRequestParameter("id", ID_FOR_TUSER_STRING);
+        addRequestParameter("firstName", FIRST_NAME_FOR_TUSER);
+        addRequestParameter("dateOfBirth", DateFormat.getDateInstance().format(DATE_OF_BIRTHDAY_FOR_TUSER));
+        addRequestParameter("okButton", OK_OPTION);
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute(ERROR_ATTRIBUTE);
+        assertNotNull("The session scope must have a error message", errorMessage);
+    }
+
+    public void testEditEmptyDateOfBirth() {
+        addRequestParameter("id", ID_FOR_TUSER_STRING);
+        addRequestParameter("firstName", FIRST_NAME_FOR_TUSER);
+        addRequestParameter("lastName", LAST_NAME_FOR_TUSER);
+        addRequestParameter("okButton", OK_OPTION);
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute(ERROR_ATTRIBUTE);
+        assertNotNull("The session scope must have a error message", errorMessage);
+    }
+
+    public void testEditInvalidDate() {
+        addRequestParameter("id", ID_FOR_TUSER_STRING);
+        addRequestParameter("firstName", FIRST_NAME_FOR_TUSER);
+        addRequestParameter("lastName", LAST_NAME_FOR_TUSER);
+        addRequestParameter("dateOfBirth", "#####");
+        addRequestParameter("okButton", OK_OPTION);
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute(ERROR_ATTRIBUTE);
+        assertNotNull("The session scope must have a error message", errorMessage);
     }
 }
