a435233d71761af8caf73012d2e35bf48dd4bdf7
kostintyn.yavorovenko <kostintyn.yavorovenko@nure.ua>
2018-12-16 11:15:10 +0300

    RequestServer#createReply implemented

--- usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/agent/RequestServer.java
+++ usermanagement/src/main/java/ua/nure/kn/yavorovenko/usermanagement/agent/RequestServer.java
@@ -3,10 +3,19 @@ package ua.nure.kn.yavorovenko.usermanagement.agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import ua.nure.kn.yavorovenko.usermanagement.User;
+import ua.nure.kn.yavorovenko.usermanagement.db.DaoFactory;
+import ua.nure.kn.yavorovenko.usermanagement.db.DatabaseException;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class RequestServer extends CyclicBehaviour {
+
+    private static final String STRING_SEPARATOR = ",";
+    private static final int VALID_COUNT_TOKENS_IN_MSG = 2;
+    private static final int SIZE_OF_EMPTY_LIST = 0;
 
     @Override
     public void action() {
@@ -28,6 +37,30 @@ public class RequestServer extends CyclicBehaviour {
     }
 
     private ACLMessage createReply(ACLMessage message) {
-        return null;
+        ACLMessage reply = message.createReply();
+        String content = message.getContent();
+        StringTokenizer tokenizer = new StringTokenizer(content, STRING_SEPARATOR);
+        if (tokenizer.countTokens() == VALID_COUNT_TOKENS_IN_MSG) {
+            String firstName = tokenizer.nextToken();
+            String lastName = tokenizer.nextToken();
+            Collection<User> users = null;
+            try {
+                users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            } catch (DatabaseException e) {
+                e.printStackTrace();
+                users = new ArrayList<>(SIZE_OF_EMPTY_LIST);
+            }
+
+            StringBuffer buffer = new StringBuffer();
+            for (Iterator<User> it = users.iterator(); it.hasNext();) {
+                User user = it.next();
+                buffer.append(user.getId()).append(STRING_SEPARATOR);
+                buffer.append(user.getFirstName()).append(STRING_SEPARATOR);
+                buffer.append(user.getLastName()).append(STRING_SEPARATOR);
+            }
+            reply.setContent(buffer.toString());
+        }
+
+        return reply;
     }
 }
